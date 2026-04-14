1a77c6ac98aed81b00e869d721e437b72a21e8a8
Uzeda <uzeda.dev@gmail.com>
2026-04-14 12:15:35 -0300

    [chore] improve telegram notifier logging

--- src/components/Notifier.js
+++ src/components/Notifier.js
@@ -2,9 +2,18 @@
 
 const config = require('../config');
 const axios = require('axios');
+const $logger = require('./Logger');
 
 exports.sendNotification = async (msg) => {
+    const startedAt = new Date().toISOString();
+    $logger.info(
+        `[Telegram] sendMessage iniciada em ${startedAt} (${msg.length} caracteres)`
+    );
     const apiUrl = `https://api.telegram.org/bot${config.telegramToken}/sendMessage?chat_id=${config.telegramChatID}&text=`;
     const encodedMsg = encodeURIComponent(msg);
-    return await axios.get(apiUrl + encodedMsg, { timeout: 5000 });
+    const res = await axios.get(apiUrl + encodedMsg, { timeout: 5000 });
+    $logger.info(
+        `[Telegram] sendMessage concluída em ${new Date().toISOString()} (HTTP ${res.status})`
+    );
+    return res;
 };
